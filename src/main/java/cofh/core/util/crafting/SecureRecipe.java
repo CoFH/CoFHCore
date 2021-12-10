f69229d41100888ec50ff34628063d867936c940
King Lemming <kinglemming@gmail.com>
2021-12-09 20:48:48 -0800

    Cleanup.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/core/util/crafting/SecureRecipe.java
+++ src/main/java/cofh/core/util/crafting/SecureRecipe.java
@@ -1,6 +1,5 @@
 package cofh.core.util.crafting;
 
-import cofh.core.event.CoreCommonSetupEvents;
 import cofh.lib.util.control.ISecurable.AccessMode;
 import cofh.lib.util.helpers.SecurityHelper;
 import cofh.lib.util.references.ItemTagsCoFH;
@@ -25,9 +24,6 @@ public class SecureRecipe extends SpecialRecipe {
     @Override
     public boolean matches(CraftingInventory inv, World worldIn) {
 
-        if (!CoreCommonSetupEvents.getTagsInitialized()) {
-            return false;
-        }
         Ingredient ingredientSecure = Ingredient.of(ItemTagsCoFH.LOCKS);
         Ingredient ingredientSecurable = Ingredient.of(ItemTagsCoFH.SECURABLE);
 
