00de8c1fb8566269710abda8c0bcfc4ea35b92c3
King Lemming <kinglemming@gmail.com>
2022-06-14 00:06:22 -0700

    Update Subhitbox support.

--- src/main/java/cofh/core/event/CoreClientEvents.java
+++ src/main/java/cofh/core/event/CoreClientEvents.java
@@ -3,22 +3,34 @@ package cofh.core.event;
 import cofh.core.init.CoreConfig;
 import cofh.lib.client.renderer.entity.ITranslucentRenderer;
 import cofh.lib.util.Utils;
+import cofh.lib.util.raytracer.VoxelShapeRayTraceResult;
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import net.minecraft.block.Block;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.renderer.ActiveRenderInfo;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.ListNBT;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.vector.Matrix4f;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.IFormattableTextComponent;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.DrawHighlightEvent;
 import net.minecraftforge.client.event.RenderTooltipEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -163,4 +175,39 @@ public class CoreClientEvents {
         ITranslucentRenderer.renderTranslucent(event.getMatrixStack(), event.getPartialTicks(), event.getContext(), event.getProjectionMatrix());
     }
 
+    @SubscribeEvent (priority = EventPriority.LOW)
+    public static void renderSubHitboxes(DrawHighlightEvent.HighlightBlock event) {
+
+        BlockRayTraceResult hit = event.getTarget();
+        if (hit instanceof VoxelShapeRayTraceResult) {
+            VoxelShapeRayTraceResult voxelHit = (VoxelShapeRayTraceResult) hit;
+            MatrixStack stack = event.getMatrix();
+            BlockPos pos = voxelHit.getBlockPos();
+            event.setCanceled(true);
+
+            stack.pushPose();
+            stack.translate(pos.getX(), pos.getY(), pos.getZ());
+
+            bufferShapeHitBox(stack, event.getBuffers(), event.getInfo(), voxelHit.shape);
+
+            stack.popPose();
+        }
+    }
+
+    // region HELPERS
+    private static void bufferShapeHitBox(MatrixStack pStack, IRenderTypeBuffer buffers, ActiveRenderInfo renderInfo, VoxelShape shape) {
+
+        Vector3d eye = renderInfo.getPosition();
+        pStack.translate((float) -eye.x, (float) -eye.y, (float) -eye.z);
+        bufferShapeOutline(buffers.getBuffer(RenderType.lines()), pStack.last().pose(), shape, 0.0F, 0.0F, 0.0F, 0.4F);
+    }
+
+    private static void bufferShapeOutline(IVertexBuilder builder, Matrix4f mat, VoxelShape shape, float r, float g, float b, float a) {
+
+        shape.forAllEdges((x1, y1, z1, x2, y2, z2) -> {
+            builder.vertex(mat, (float) x1, (float) y1, (float) z1).color(r, g, b, a).endVertex();
+            builder.vertex(mat, (float) x2, (float) y2, (float) z2).color(r, g, b, a).endVertex();
+        });
+    }
+    // endregion
 }
