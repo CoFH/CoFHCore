cd7b34f9525fc1b841a71808be86567c407f6df7
King Lemming <kinglemming@gmail.com>
2022-11-24 10:41:38 -0800

    Update ArcheryEvents.java

--- src/main/java/cofh/core/event/ArcheryEvents.java
+++ src/main/java/cofh/core/event/ArcheryEvents.java
@@ -7,7 +7,6 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
