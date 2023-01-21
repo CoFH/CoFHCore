a3018a9d610bcbcb98294b3e3e2d9bc2ff52f434
Hekera <40907168+Hekera@users.noreply.github.com>
2023-01-21 02:29:53 -0500

    chilled rework

--- src/main/java/cofh/core/effect/ChilledMobEffect.java
+++ src/main/java/cofh/core/effect/ChilledMobEffect.java
@@ -2,12 +2,10 @@ package cofh.core.effect;
 
 import cofh.lib.effect.CustomParticleMobEffect;
 import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 
 import static cofh.core.init.CoreParticles.FROST;
-import static cofh.lib.util.Constants.UUID_EFFECT_CHILLED_MOVEMENT_SPEED;
 
 public class ChilledMobEffect extends CustomParticleMobEffect {
 
@@ -19,16 +17,17 @@ public class ChilledMobEffect extends CustomParticleMobEffect {
     @Override
     public double getAttributeModifierValue(int amplifier, AttributeModifier modifier) {
 
-        if (modifier.getId() == UUID_EFFECT_CHILLED_MOVEMENT_SPEED) {
-            return Math.max(-0.90D, super.getAttributeModifierValue(amplifier, modifier));
-        }
-        return super.getAttributeModifierValue(amplifier, modifier);
+        return Math.max(-0.90D, super.getAttributeModifierValue(amplifier, modifier));
+        //if (modifier.getId() == UUID_EFFECT_CHILLED_MOVEMENT_SPEED) {
+        //    return Math.max(-0.90D, super.getAttributeModifierValue(amplifier, modifier));
+        //}
+        //return super.getAttributeModifierValue(amplifier, modifier);
     }
 
     @Override
     public ParticleOptions getParticle() {
 
-        return (SimpleParticleType) FROST.get();
+        return FROST.get();
     }
 
 }
