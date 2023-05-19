e27fa644729b367551a139caac6dce7d38d9dda5
King Lemming <kinglemming@gmail.com>
2023-05-19 13:11:19 -0700

    Update Utils.java

--- src/main/java/cofh/lib/util/Utils.java
+++ src/main/java/cofh/lib/util/Utils.java
@@ -351,7 +351,7 @@ public class Utils {
 
     public static boolean dropDismantleStackIntoWorld(ItemStack stack, Level world, BlockPos pos) {
 
-        if (stack.isEmpty()) {
+        if (stack.isEmpty() || Utils.isClientWorld(world)) {
             return false;
         }
         float f = 0.3F;
