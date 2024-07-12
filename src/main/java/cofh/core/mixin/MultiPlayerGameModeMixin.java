99b5bf04e07d4eeae23258b4cba9dd3ae1deca79
King Lemming <kinglemming@gmail.com>
2024-07-12 01:09:04 -0700

    Comment out broken mixin.

--- src/main/java/cofh/core/mixin/MultiPlayerGameModeMixin.java
+++ src/main/java/cofh/core/mixin/MultiPlayerGameModeMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,6 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class MultiPlayerGameModeMixin {
 
     @Shadow
+    @Final
     public Minecraft minecraft;
 
     @Shadow
